c47d9cf1c397171a3db4253cf240e26e7032fca3
Akalanka0 <akalankasenanayake88@gmail.com>
2026-03-09 23:25:36 +0530

    Test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://todoweb-production-4250.up.railway.app',
         changeOrigin: true
       }
     }
